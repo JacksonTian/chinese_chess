aa4df3f8a5bbda70ecb76de621dfdebaf40635ac
Jackson Tian <shyvo1987@gmail.com>
2023-10-15 14:03:46 +0800

    improve test coverage for game basisi

--- assets/replay.js
+++ assets/replay.js
@@ -1,5 +1,7 @@
 // 将棋谱与坐标系进行转换的工具方法
 
+import Game from "./game.js";
+
 export function getRoleByName(name) {
     const mapping = {
         '车': 'che',
@@ -106,170 +108,101 @@ export function getDistance(value) {
     return distance[value];
 }
 
-export function makeStep(find, newX, newY) {
-    // 特殊记谱情况：https://www.zhihu.com/question/470070915/answer/2748414695
-    const { player, role } = find;
-    const name = Game.getName(find);
-    let direction;
-    let end;
-    if (player === 'red') {
-        if (find.y === newY) {
-            direction = '平';
-            end = getX(player, newX);
-        } else if (find.y > newY) {
-            direction = '进';
-            if (role === 'ma') {
-                end = getX(player, newX);
-            } else if (role === 'pao') {
-                end = Math.abs(newY - find.y);
-            } else if (role === 'che') {
-                end = Math.abs(newY - find.y);
-            } else if (role === 'shi') {
-                end = getX(player, newX);
-            }
-        } else {
-            direction = '退';
-            if (role === 'ma' || role === 'shi') {
-                end = getX(player, newX);
-            } else if (role === 'pao') {
-                end = Math.abs(newY - find.y);
-            } else if (role === 'che') {
-                end = Math.abs(newY - find.y);
-            }
-        }
-
-        return `${name}${getX(player, find.x)}${direction}${end}`;
-    } else {
-        if (find.y === newY) {
-            direction = '平';
-            end = getX(player, newX);
-        } else if (find.y > newY) {
-            direction = '退';
-            if (role === 'ma') {
-                end = getX(player, newX);
-            } else if (role === 'pao') {
-                end = Math.abs(newY - find.y);
-            } else if (role === 'che') {
-                end = Math.abs(newY - find.y);
-            }
-        } else {
-            direction = '进';
-            if (role === 'ma') {
-                end = getX(player, newX);
-            } else if (role === 'pao') {
-                end = Math.abs(newY - find.y);
-            } else if (role === 'che') {
-                end = Math.abs(newY - find.y);
-            }
-        }
-
-        return `${name}${getX(player, find.x)}${direction}${end}`;
-    }
-}
-
-function move(game, record) {
+/**
+ * 
+ * @param {Game} game 
+ * @param {String} record 
+ */
+export function move(game, record) {
     const [displayName, start, direction, end] = record.split('');
     const role = getRoleByName(displayName);
-    if (game.counter % 2 === 0) {
-        const player = 'red';
-        // 红方走
-        const find = game.pieces.find((item) => {
-            return (item.role === role && item.player === player && getPosition(start) === item.x);
-        });
-        if (!find) {
-            throw new Error(`${record} not found`);
-        }
-        let x = find.x;
-        let y = find.y;
-        if (direction === '平') {
-            x = getPosition(end);
-        } else if (direction === '进') {
-            if (role === 'ma') {
-                if (Math.abs(find.x - getPosition(end)) === 2) {
+    const player = game.getCurrentPlayer();
+    const find = game.pieces.find((item) => {
+        return (item.role === role && item.player === player && getPosition(start) === item.x);
+    });
+    if (!find) {
+        throw new Error(`${record} not found`);
+    }
+    let x = find.x;
+    let y = find.y;
+    if (direction === '平') {
+        x = getPosition(end);
+    } else {
+        if (player === 'red') {
+            if (direction === '进') {
+                if (role === 'ma') {
+                    if (Math.abs(find.x - getPosition(end)) === 2) {
+                        y = find.y - 1;
+                    } else {
+                        y = find.y - 2;
+                    }
+                    x = getPosition(end);
+                } else if (role === 'xiang') {
+                    x = getPosition(end);
+                    y = find.y - 2;
+                } else if (role === 'shi') {
+                    x = getPosition(end);
                     y = find.y - 1;
                 } else {
-                    y = find.y - 2;
+                    y = find.y - getDistance(end);
                 }
-                x = getPosition(end);
-            } else if (role === 'xiang') {
-                x = getPosition(end);
-                y = find.y - 2;
-            } else if (role === 'shi') {
-                x = getPosition(end);
-                y = find.y - 1;
-            } else {
-                y = find.y - getDistance(end);
-            }
-        } else if (direction === '退') {
-            if (role === 'ma') {
-                if (Math.abs(find.x - getPosition(end)) === 2) {
+            } else if (direction === '退') {
+                if (role === 'ma') {
+                    if (Math.abs(find.x - getPosition(end)) === 2) {
+                        y = find.y + 1;
+                    } else {
+                        y = find.y + 2;
+                    }
+                    x = getPosition(end);
+                } else if (role === 'xiang') {
+                    y = find.y + 2;
+                    x = getPosition(end);
+                } else if (role === 'shi') {
+                    x = getPosition(end);
                     y = find.y + 1;
                 } else {
-                    y = find.y + 2;
+                    y = find.y + getDistance(end);
                 }
-                x = getPosition(end);
-            } else if (role === 'xiang') {
-                y = find.y + 2;
-                x = getPosition(end);
-            } else if (role === 'shi') {
-                x = getPosition(end);
-                y = find.y + 1;
-            } else {
-                y = find.y + getDistance(end);
             }
         } else {
-            throw new Error(`${record} not found`);
-        }
-
-        game.tryMove([find.x, find.y], [x, y]);
-    } else {
-        // 黑方走
-        const player = 'black';
-        const find = game.pieces.find((item) => {
-            return (item.role === role && item.player === player && getPosition(start) === item.x);
-        });
-        if (!find) {
-            throw new Error(`${record} not found`);
-        }
-        let x = find.x;
-        let y = find.y;
-        if (direction === '进') {
-            if (role === 'ma') {
-                if (Math.abs(find.x - getPosition(end)) === 2) {
+            // 黑方走
+            if (direction === '进') {
+                if (role === 'ma') {
+                    if (Math.abs(find.x - getPosition(end)) === 2) {
+                        y = find.y + 1;
+                    } else {
+                        y = find.y + 2;
+                    }
+                    x = getPosition(end);
+                } else if (role === 'xiang') {
+                    y = find.y + 2;
+                    x = getPosition(end);
+                } else if (role === 'shi') {
+                    x = getPosition(end);
                     y = find.y + 1;
                 } else {
-                    y = find.y + 2;
+                    y = find.y + Number(end);
                 }
-                x = getPosition(end);
-            } else if (role === 'xiang') {
-                y = find.y + 2;
-                x = getPosition(end);
-            } else if (role === 'shi') {
-                x = getPosition(end);
-                y = find.y + 1;
-            } else {
-                y = find.y + Number(end);
-            }
-        } else if (direction === '退') {
-            if (role === 'ma') {
-                if (Math.abs(find.x - getPosition(end)) === 2) {
+            } else if (direction === '退') {
+                if (role === 'ma') {
+                    if (Math.abs(find.x - getPosition(end)) === 2) {
+                        y = find.y - 1;
+                    } else {
+                        y = find.y - 2;
+                    }
+                    x = getPosition(end);
+                } else if (role === 'xiang') {
+                    y = find.y - 2;
+                    x = getPosition(end);
+                } else if (role === 'shi') {
+                    x = getPosition(end);
                     y = find.y - 1;
                 } else {
-                    y = find.y - 2;
+                    y = find.y - Number(end);
                 }
-                x = getPosition(end);
-            } else if (role === 'xiang') {
-                y = find.y - 2;
-                x = getPosition(end);
-            } else if (role === 'shi') {
-                x = getPosition(end);
-                y = find.y - 1;
-            } else {
-                y = find.y - Number(end);
             }
-        } else if (direction === '平') {
-            x = getPosition(end);
         }
-        game.tryMove([find.x, find.y], [x, y]);
     }
+
+    game.tryMove([find.x, find.y], [x, y]);
 }
--- test/replay.test.js
+++ test/replay.test.js
@@ -1,5 +1,7 @@
 import assert from "assert";
-import { getRoleByName, getX } from "../assets/replay.js";
+import { getRoleByName, getX, getDistance, getPosition, move } from "../assets/replay.js";
+import Game from "../assets/game.js";
+import Piece from "../assets/piece.js";
 
 describe('replay.js', () => {
     it('getRoleByName should ok', () => {
@@ -25,6 +27,90 @@ describe('replay.js', () => {
         assert.equal(getX('black', 6), '7');
         assert.equal(getX('black', 7), '8');
         assert.equal(getX('black', 8), '9');
+    });
+
+    it('getDistance should ok', () => {
+        assert.equal(getDistance('一'), 1);
+    });
+
+    it('getPosition, should ok', () => {
+        assert.equal(getPosition('一'), 8);
+    });
+
+    it('move() for pao should ok', () => {
+        const game = new Game();
+        game.initGame();
+        move(game, '炮二平五');
+        assert.deepStrictEqual(game.find(4, 7), new Piece('pao', 'red', 4, 7));
+        move(game, '炮2平6');
+        assert.deepStrictEqual(game.find(5, 2), new Piece('pao', 'black', 5, 2));
+        move(game, '炮八进二');
+        assert.deepStrictEqual(game.find(1, 5), new Piece('pao', 'red', 1, 5));
+        move(game, '炮8进2');
+        assert.deepStrictEqual(game.find(7, 4), new Piece('pao', 'black', 7, 4));
+        move(game, '炮八退二');
+        assert.deepStrictEqual(game.find(1, 7), new Piece('pao', 'red', 1, 7));
+        move(game, '炮8退2');
+        assert.deepStrictEqual(game.find(7, 2), new Piece('pao', 'black', 7, 2));
+    });
+
+    it('move() for ma should ok', () => {
+        const game = new Game();
+        game.initGame();
+        move(game, '马二进三');
+        assert.deepStrictEqual(game.find(6, 7), new Piece('ma', 'red', 6, 7));
+        move(game, '马2进3');
+        assert.deepStrictEqual(game.find(2, 2), new Piece('ma', 'black', 2, 2));
+        move(game, '马三退二');
+        assert.deepStrictEqual(game.find(7, 9), new Piece('ma', 'red', 7, 9));
+        move(game, '马3退2');
+        assert.deepStrictEqual(game.find(1, 0), new Piece('ma', 'black', 1, 0));
+        move(game, '马二进三');
+        assert.deepStrictEqual(game.find(6, 7), new Piece('ma', 'red', 6, 7));
+        move(game, '马2进3');
+        assert.deepStrictEqual(game.find(2, 2), new Piece('ma', 'black', 2, 2));
+        move(game, '马三退五');
+        assert.deepStrictEqual(game.find(4, 8), new Piece('ma', 'red', 4, 8));
+        move(game, '马3退5');
+        assert.deepStrictEqual(game.find(4, 1), new Piece('ma', 'black', 4, 1));
+        move(game, '马五进三');
+        assert.deepStrictEqual(game.find(6, 7), new Piece('ma', 'red', 6, 7));
+        move(game, '马5进3');
+        assert.deepStrictEqual(game.find(2, 2), new Piece('ma', 'black', 2, 2));
+    });
+
+    it('move() for xiang should ok', () => {
+        const game = new Game();
+        game.initGame();
+        move(game, '象三进五');
+        assert.deepStrictEqual(game.find(4, 7), new Piece('xiang', 'red', 4, 7));
+        move(game, '象3进5');
+        assert.deepStrictEqual(game.find(4, 2), new Piece('xiang', 'black', 4, 2));
+        move(game, '象五退三');
+        assert.deepStrictEqual(game.find(6, 9), new Piece('xiang', 'red', 6, 9));
+        move(game, '象5退3');
+        assert.deepStrictEqual(game.find(2, 0), new Piece('xiang', 'black', 2, 0));
+    });
+
+    it('move() for shi should ok', () => {
+        const game = new Game();
+        game.initGame();
+        move(game, '士四进五');
+        assert.deepStrictEqual(game.find(4, 8), new Piece('shi', 'red', 4, 8));
+        move(game, '士4进5');
+        assert.deepStrictEqual(game.find(4, 1), new Piece('shi', 'black', 4, 1));
+        move(game, '士五退四');
+        assert.deepStrictEqual(game.find(5, 9), new Piece('shi', 'red', 5, 9));
+        move(game, '士5退4');
+        assert.deepStrictEqual(game.find(3, 0), new Piece('shi', 'black', 3, 0));
+    });
+
+    it('move() should ok', () => {
+        const game = new Game();
+        game.initGame();
+        assert.throws(() => {
+            move(game, '士三进五');
+        }, /士三进五 not found/);
     });
 });
 
